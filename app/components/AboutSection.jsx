e36f64c3b7a4b7294ef1e16e5c67164b856acc62
Larryking007 <ajikobilanre007@gmail.com>
2023-10-01 23:41:09 +0100

    About Section

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -1,15 +1,92 @@
-import React from "react";
+"use client";
+import React, { useState, useTransition } from "react";
+import TabButton from "./TabButton";
 import Image from "next/image";
 
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Javascript</li>
+        <li>React</li>
+        <li>Git</li>
+        <li>OOP</li>
+        <li>HTML</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Federal University, of Tecnology, Akure</li>
+        <li>Zuri Academy</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certs",
+    id: "certs",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Programming with Javascript</li>
+        <li>React</li>
+      </ul>
+    ),
+  },
+];
+
 const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
   return (
     <section className="text-white">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16">
         <Image src={"/images/541953.png"} width={500} height={500} />
-      </div>
-      <div>
-        <h2>About Me</h2>
-        <p></p>
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I'm a frontend developer with a passion for creating interactive and
+            responsive web application. I have experience working with
+            Javascript, React, Redux, HTML, CSS, Git, Jquery and OOP. I am a
+            quick learner and I am always looking to expand my knowlege and
+            skill set. I am a team player and I am excited to work with others
+            to create amazing applications.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              Skills
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              Education
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certs")}
+              active={tab === "certs"}
+            >
+              Certs
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
       </div>
     </section>
   );
