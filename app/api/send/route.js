184bed8facea818061f8947fc59c918dd67311e3
mansamusa <ajikobilanre007@gmail.com>
2025-05-13 05:42:52 +0100

    Add JSX support in jsconfig.json and update devDependencies for TypeScript types

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,29 +1,23 @@
-// import { EmailTemplate } from "../../../components/EmailTemplate";
-import { NextResponse } from "next/server";
+import { EmailTemplate } from "../../../components/EmailTemplate";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
-export async function POST(req, res) {
-  const { body } = req.json();
-  const { email, subject, message } = body;
+
+export async function POST() {
   try {
-    const data = await resend.emails.send({
+    const { data, error } = await resend.emails.send({
       from: "Mansa <ajikobilanre@yahoo.com>",
       to: ["ajikobilanre007@gmail.com"],
       subject: "Hello world",
-      react: (
-        <>
-          <h1>{subject}</h1>
-          <p>Thank you for contacting us! </p>
-          <p>New message submitted:</p>
-          <p>{message}</p>
-        </>
-      ),
+      react: EmailTemplate({ firstName: "John" }),
     });
 
-    return NextResponse.json(data);
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json(data);
   } catch (error) {
-    return NextResponse.json({ error });
+    return Response.json({ error }, { status: 500 });
   }
 }
